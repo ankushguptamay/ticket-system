c26e2b3c9c94bf73d39c47b4ed1af763ab8504a3
Ankush Gupta <ankushgupta9675@gmail.com>
2024-02-05 13:17:48 +0530

    some change

--- Middlewares/uploadAttachment.js
+++ Middlewares/uploadAttachment.js
@@ -14,7 +14,7 @@ const filter = (req, file, cb) => {
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
         console.log(__dirname);
-        const filePath = path.join(`${__dirname}../Resource`);
+        const filePath = path.join(`${__dirname}/../Resource`);
         if (file.fieldname === "attachment") {
             cb(null, filePath);
         }
